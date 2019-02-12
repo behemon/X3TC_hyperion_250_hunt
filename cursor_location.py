f647fc1f4a81f75b992065151d02e40a18fd7298
alexy <pluma54@gmail.com>
2019-02-12 07:58:33 +0200

    1.0.1

--- cursor_location.py
+++ cursor_location.py
@@ -7,11 +7,12 @@ def pyautogui_test():
         while True:
             x, y = pyautogui.position()
             positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-            print positionStr,
-            print '\b' * (len(positionStr) + 2),
+            print (positionStr,end="")
+            print ('\b' * (len(positionStr) + 2),end="")
             sys.stdout.flush()
     except KeyboardInterrupt:
-        print '\n'
+        print ('\n')
+
 
 if (__name__ == "__main__"):
     pyautogui_test()
